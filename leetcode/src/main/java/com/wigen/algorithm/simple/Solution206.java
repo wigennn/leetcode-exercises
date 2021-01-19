6583ed7b13c4ed207c3fab80a8ef26ebf60da327
wangwq <wangweiqing@yunzhangfang.com>
2021-01-19 14:01:30 +0800

    206 反转链表

---
+++ leetcode/src/main/java/com/wigen/algorithm/simple/Solution206.java
@@ -0,0 +1,58 @@
+package com.wigen.algorithm.simple;
+
+/**
+ * <p>
+ * 206. 反转链表
+ * 反转一个单链表。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ * </p>
+ *
+ * @author wangwq
+ */
+public class Solution206 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        reverseList(head);
+    }
+}
