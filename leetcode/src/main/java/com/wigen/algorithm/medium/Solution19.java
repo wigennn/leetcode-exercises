0ab96fad3127ed9cc04c5db6343d46c6f1a41ff9
wangwq <wangweiqing@yunzhangfang.com>
2021-01-19 11:48:29 +0800

    18 链表删除倒数节点

---
+++ leetcode/src/main/java/com/wigen/algorithm/medium/Solution19.java
@@ -0,0 +1,69 @@
+package com.wigen.algorithm.medium;
+
+/**
+ * <p>
+ * 19. 删除链表的倒数第 N 个结点
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * <p>
+ * 进阶：你能尝试使用一趟扫描实现吗？
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5], n = 2
+ * 输出：[1,2,3,5]
+ * 示例 2：
+ * <p>
+ * 输入：head = [1], n = 1
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：head = [1,2], n = 1
+ * 输出：[1]
+ * </p>
+ *
+ * @author wangwq
+ */
+public class Solution19 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode preNode = head;
+        ListNode curNode = head;
+        while (head.next != null) {
+            if (n <= 0) curNode = curNode.next;
+            head = head.next;
+            n--;
+        }
+        if (n == 1) return preNode.next;
+        curNode.next = curNode.next == null ? null : curNode.next.next;
+        return preNode;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        removeNthFromEnd(head, 5);
+    }
+}
